679d82c73b7f2fc5e0fa00d4420351c01e9b7112
Bugra <bugraa@live.com>
2012-09-02 07:45:13 -0400

    Removing Schmitt Trigger

--- fft_method.m
+++ fft_method.m
@@ -11,10 +11,7 @@ fc = 70e6;
 lpf = ones(1,16);
 lpf = lpf/length(lpf);
 %noise coefficient
-n = 0.1;
-%Threshold values for Schmitt Trigger
-thr_high = 0.5;
-thr_low = thr_high * 0.8;
+n = 0;
 %First Signal
 x1= cos(2 * pi * fc * t) + (n * randn(1, length(t)));
 %Delay
@@ -22,20 +19,14 @@ delay = 0.5e-9;
 %Delayed Signal 
 x2 = cos(2 * pi * fc *(t + delay)) + (n * randn(1, length(t)));
 
-figure()
-subplot(2, 1, 1);
-plot(t, x1, 'g')
-title('Original Signal')
-subplot(2, 1, 2);
-plot(t, x2, 'r')
-title('Delayed Signal')
-
 numberofPoints = ceil((length(t) + 1) / 2);% numberofPoints is 126
 f = ((0:numberofPoints - 1) * fs) / length(t);
 
 figure(1)
 subplot(2, 1, 1),plot(x1)
+title('Original Signal')
 subplot(2, 1, 2),plot(x2)
+title('Delayed Signal')
 fftFirstSignal = fft(x1);
 fftSecondSignal = fft(x2);
 
